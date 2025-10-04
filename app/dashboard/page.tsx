98d479aa9d52e91731aef75979f4483e3946c786
SKAHLO <egbunaoscar@gmail.com>
2025-10-04 10:17:48 +0100

    dashboard logging 2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import dynamic from 'next/dynamic'
+import { useState } from 'react';
 
 const DashboardLayout = dynamic(
   () => import('@/components/dashboard-layout').then(mod => ({ default: mod.DashboardLayout })),
